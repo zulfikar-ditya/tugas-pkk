cff2619f28f730b5f312af06c898a29445aa3c98
Zulfikar Ditya <zulfikarditya.work@gmail.com>
2020-11-17 22:17:10 +0700

    my bill list

--- my_app/urls.py
+++ my_app/urls.py
@@ -6,5 +6,6 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('my-bill/', views.myBill, name='myBill'),
     path('register/', views.register, name='register'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 
 from .forms import RegisterForm
+from .models import Tagihan, Uses
 
 
 def index(request):
@@ -22,6 +25,28 @@ def register(request):
     else:
         form = RegisterForm()
     return render(request, 'registration/login.html', {'page': 'register', 'form': form})
+
+
+@login_required
+def myBill(request):
+    uses = Uses.objects.all()
+    tagihan = Tagihan.objects.all()
+    for i in uses:
+        try:
+            tagihan_item = Tagihan.objects.get(uses=i.id)
+        except:
+            new = Tagihan()
+            new.custommer = i.custommer
+            new.uses = i
+            new.month = i.month
+            new.year = i.year
+            new.sum_meter = int(i.meter_end) - int(i.meter_start)
+            new.save()
+    data = Tagihan.objects.filter(custommer=request.user, status=False)
+    page = Paginator(data, 50)
+    PageNum = request.GET.get('page')
+    data_result = page.get_page(PageNum)
+    return render(request, 'home/my-bill.html', {'data': data_result})
 
 
 def dont_have_acces(request):
